764b80a09e95d887f5e837f7d7246dc8b7638860
Gabriel Oliveira <gabriel.alves9400@hotmail.com>
2021-09-15 02:20:30 -0300

    Add Path para a pasta public

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,20 @@
 //Requisições do Pacote Express
-import express from "express";
+import express, {Request,Response} from "express";
 import mainRouter from './routes/index';
+import painelRouter from './routes/painel';
+import path from 'path';
 const server = express();
-server.use(mainRouter);
-//ler os aquivos da pasta public
-/*server.use(express.static(path.join(__dirname, 'public')));*/
+
+//Deixa os arquivos da pasta public serem acessados --- dirname serve para pegar o diretorio raiz
+server.use(express.static(path.join(__dirname, '../public')));
+//Requer as rotas principais
+server.use('/',mainRouter);
+
+//Requer as rotas do painel
+server.use('/painel', painelRouter);
+
+//Rota de erro 404
+server.use((req:Request, res:Response )=>{
+    res.status(404).send('Pagina Não Encontrada');
+});
 server.listen(3000);
